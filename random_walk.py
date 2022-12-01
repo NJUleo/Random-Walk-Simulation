2306acfaa49bbfa57537dc7acf8583f8c0986b49
NJUleo <1250925329@qq.com>
2022-12-01 09:17:51 -0500

    refactor: extract the compute method; add a new compute method

--- random_walk.py
+++ random_walk.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-sigma = 0.712022
+sigma = 0.4935
 
 
-def single_random_walk(step_num):
+# generate the result of a random walk of a cretain step.
+def single_random_walk(start_point, step_num):
     # simulate a single random walk and return the value
     # num: number of steps taken
 
@@ -11,28 +12,53 @@ def single_random_walk(step_num):
     rng = np.random.default_rng()  # a new generator, used to get a new normal distribution.
     steps = rng.normal(0, sigma, step_num)
 
-    cur = 0  # current location, initialized as x_0, which is 0
+    cur = start_point  # current location, initialized as x_0, which is 0
     x = [cur]  # x_i, i = 0...1000
     for step in steps:
         cur += step
         x.append(cur)
 
     # compute result
-    x_average = sum(x) / 1000
+    return x
+
+
+def compute_result_2(x):
     numerator = 0
     denominator = 0
-    for i in range(1, 1001):
+    max_res = float('-inf')
+    for i in range(len(x) - 1):
+        numerator += x[i] * x[i + 1]
+        denominator += x[i] ** 2
+        cur = numerator / denominator
+        max_res = max(max_res, cur)
+    return max_res
+
+
+def compute_result(x):
+    x_average = sum(x) / len(x)
+    numerator = 0
+    denominator = 0
+    for i in range(1, len(x)):
         numerator += (x[i] - x_average) * (x[i - 1] - x_average)
         denominator += (x[i] - x_average) ** 2
     return numerator / denominator
 
 
-def simulate(simulation_num, step_num):
+# return the top 1%, 5%, 10%
+def get_top(x):
+    return (
+        x[len(x) * 1 // 100],
+        x[len(x) * 5 // 100],
+        x[len(x) * 10 // 100],
+    )
+
+
+def simulate(simulation_num, start_point, step_num, compute_func):
     # simulation_num: number of simulations
     # step_num: number of steps in each simulation
     simulation_res = []
     for _ in range(simulation_num):
-        simulation_res.append(single_random_walk(step_num))
+        simulation_res.append(compute_func(single_random_walk(start_point, step_num)))
     simulation_res.sort(reverse=True)
     return (
         simulation_res[simulation_num * 1 // 100],
@@ -41,5 +67,5 @@ def simulate(simulation_num, step_num):
 
 
 if __name__ == '__main__':
-    res = simulate(10000, 1000)
+    res = simulate(100000, 4.849, 384, compute_result_2)
     print("1%: {}, 5%: {}, 10%: {}".format(res[0], res[1], res[2]))
